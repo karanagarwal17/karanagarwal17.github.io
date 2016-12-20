77dc1749194ce48492549f2e2a201f9a4821298e
karanagarwal17 <karanagarwal1702@gmail.com>
2016-12-20 18:15:08 +0530

    style for the skills section

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,7 +1,81 @@
 /* SKILLS ANIMATION */
 
-  $('.circle').circleProgress({
+  $('.lang-c-1').circleProgress({
     value: 0.75,
+    size: 100,
+    fill: {
+      gradient: ["#3498db", "#3498db"]
+    }
+  });
+
+  $('.lang-c-2').circleProgress({
+    value: 0.60,
+    size: 100,
+    fill: {
+      gradient: ["#3498db", "#3498db"]
+    }
+  });
+
+  $('.web-c-1').circleProgress({
+    value: 0.90,
+    size: 100,
+    fill: {
+      gradient: ["#3498db", "#3498db"]
+    }
+  });
+
+  $('.web-c-2').circleProgress({
+    value: 0.80,
+    size: 100,
+    fill: {
+      gradient: ["#3498db", "#3498db"]
+    }
+  });
+
+  $('.fram-c-1').circleProgress({
+    value: 0.70,
+    size: 100,
+    fill: {
+      gradient: ["#3498db", "#3498db"]
+    }
+  });
+  $('.fram-c-2').circleProgress({
+    value: 0.65,
+    size: 100,
+    fill: {
+      gradient: ["#3498db", "#3498db"]
+    }
+  });
+  $('.lib-c-1').circleProgress({
+    value: 0.55,
+    size: 100,
+    fill: {
+      gradient: ["#3498db", "#3498db"]
+    }
+  });
+  $('.lib-c-2').circleProgress({
+    value: 0.70,
+    size: 100,
+    fill: {
+      gradient: ["#3498db", "#3498db"]
+    }
+  });
+  $('.lib-c-3').circleProgress({
+    value: 0.65,
+    size: 100,
+    fill: {
+      gradient: ["#3498db", "#3498db"]
+    }
+  });
+  $('.data-c-1').circleProgress({
+    value: 0.55,
+    size: 100,
+    fill: {
+      gradient: ["#3498db", "#3498db"]
+    }
+  });
+  $('.plat-c-1').circleProgress({
+    value: 0.65,
     size: 100,
     fill: {
       gradient: ["#3498db", "#3498db"]
