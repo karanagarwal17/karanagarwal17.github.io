e7443db0b427361a84676d16bd1f7e2812e59aaf
karanagarwal17 <karanagarwal1702@gmail.com>
2016-12-21 18:41:50 +0530

    added the project structure

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -85,19 +85,16 @@
   /*----------------------------------------------------*/
 	/*	Modal Popup
 	------------------------------------------------------*/
-   $('.item-wrap a').magnificPopup({
 
-      type:'inline',
-      fixedContentPos: false,
-      removalDelay: 300,
-      showCloseBtn: false,
-      mainClass: 'mfp-fade'
+    $('.item-wrap a').magnificPopup({
+       type:'inline',
+       fixedContentPos: false,
+       removalDelay: 200,
+       showCloseBtn: false,
+       mainClass: 'mfp-fade'
+      });
 
-   });
-
-   $(document).on('click', '.popup-modal-dismiss', function (e) {
-   	e.preventDefault();
-   	$.magnificPopup.close();
-   });
-
-	
+    $(document).on('click', '.popup-modal-dismiss', function (e) {
+    		e.preventDefault();
+    		$.magnificPopup.close();
+    });
